f2e04dc25cc65270c324288e8ca77010df8dd460
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-10 09:24:00 +0530

    created a submission

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log(secondHighest([5, 1, 2, 3, 4]));  
-console.log(secondHighest([-1, -2, -3, -4, -5]));   
-console.log(secondHighest([]));   
-console.log(secondHighest([1]));   
-console.log(secondHighest([1, 1, 1, 1, 1]));   
